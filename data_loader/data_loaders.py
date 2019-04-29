5b90fb49fe21269bef96f0b40742066ff3de2708
rpytel1 <rafal.pytel1@wp.pl>
2019-04-29 15:48:11 +0200

    Data loader working

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,8 +1,6 @@
-from torchvision import datasets, transforms
 from base import BaseDataLoader
 import pickle
 from torch.utils.data.dataset import Dataset  # For custom datasets
-import os 
 
 class STFTDataLoader(BaseDataLoader):
     """
@@ -10,26 +8,21 @@ class STFTDataLoader(BaseDataLoader):
 
     Load preprocessed data
     """
-    def __init__(self, batch_size, shuffle=False, validation_split=0.0, num_workers=1):
-        self.file_name = "train.pkl"
-        self.dataset = EdinburghDataset(self.file_name)
+    def __init__(self, data_dir, batch_size, shuffle=False, validation_split=0.0, num_workers=1):
+        self.data_dir = data_dir
+        self.dataset = EdinburghDataset(self.data_dir)
 
         super(STFTDataLoader, self).__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
 
 
 class EdinburghDataset(Dataset):
-    print("We are here: " + os.path.dirname(os.path.realpath(__file__)))
-    
-    dir_path = "./data/processed/edinburgh-noisy-speech-db/"
 
-    def __init__(self, file_name):
-        with open(self.dir_path + file_name, 'rb') as f:
+    def __init__(self, data_dir):
+        with open(data_dir, 'rb') as f:
             d = pickle.load(f)
 
         self.labels = d["targets"]
         self.attributes = d["predictors"]
-        
-        print(self.attributes.shape)
 
     def __getitem__(self, index):
         return self.attributes[index], self.labels[index]
