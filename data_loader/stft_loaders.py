ccbeb54588a1f193a8493a87ae77ea444875e2fc
rpytel1 <rafal.pytel1@wp.pl>
2019-04-29 14:53:55 +0200

    Introducing new data loader, still WIP

--- data_loader/stft_loaders.py
+++ data_loader/stft_loaders.py
@@ -1,5 +1,5 @@
-from torchvision import datasets, transforms
 from base import BaseDataLoader
+from data_loader.dataset.edinburgh_dataset import EdinburghDataset
 
 
 class STFTDataLoader(BaseDataLoader):
@@ -8,6 +8,8 @@ class STFTDataLoader(BaseDataLoader):
 
     Load preprocessed data
     """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
-        
+    def __init__(self, file_name, batch_size, shuffle=False, validation_split=0.0, num_workers=1):
+        self.file_name = file_name
+        self.dataset = EdinburghDataset(self.file_name)
+
         super(STFTDataLoader, self).__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
