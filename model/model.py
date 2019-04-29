bc1e2cdfb43b09264faaac5455ab4c5abee5247e
Gabriele Mazzola <gabriele.mazzola@techedgegroup.com>
2019-04-29 14:50:24 +0200

    add Convolutional baseline model

--- model/model.py
+++ model/model.py
@@ -35,3 +35,28 @@ class FullyConnectedBaseline(BaseModel):
         x = F.relu(self.fc2_bn(self.fc2(x)))
         x = self.fc3(x)
         return x
+
+
+class ConvolutionalBaseline(BaseModel):
+    def __init__(self, n_features, n_segments):
+        super(ConvolutionalBaseline, self).__init__()
+        self.n_features = n_features
+        self.n_segments = n_segments
+        self.conv1 = nn.Conv2d(1, 18, kernel_size=(9,8), stride=1, padding=(4, 0))
+        self.conv1_bn = nn.BatchNorm2d(18)
+        self.conv2 = nn.Conv2d(18, 30, kernel_size=(9,8), stride=1, padding=(4, 0))
+        self.conv2_bn = nn.BatchNorm2d(30)
+        self.conv3 = nn.Conv2d(30, 40, kernel_size=(9,8), stride=1, padding=(4, 0))
+        self.conv3_bn = nn.BatchNorm2d(40)
+        self.fc1 = nn.Linear(40 * n_features * n_segments, 1024)
+        self.fc1_bn = nn.BatchNorm1d(1024)
+        self.fc2 = nn.Linear(1024, n_features)
+
+    def forward(self, x):
+        x = F.relu(self.conv1_bn(self.conv1(x)))
+        x = F.relu(self.conv2_bn(self.conv2(x)))
+        x = F.relu(self.conv3_bn(self.conv3(x)))
+        x = x.view(-1, 40 * self.n_features * self.n_segments)
+        x = F.relu(self.fc1_bn(self.fc1(x)))
+        x = self.fc2(x)
+        return x
