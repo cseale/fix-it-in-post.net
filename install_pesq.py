af43fbd449da137b1afd1ee9e29e4a965cd3786f
matteop <matteo.pocchiari@hotmail.it>
2019-05-13 15:58:27 +0200

    test with pesq

--- install_pesq.py
+++ install_pesq.py
@@ -4,6 +4,6 @@ import subprocess
 # subprocess.call('git submodule update', shell=True)
 wd = os.getcwd()
 os.chdir("pesq_lib/pypesq")
-subprocess.call('python3 setup.py build_ext --inplace', shell=True)
+subprocess.call('python setup.py build_ext --inplace', shell=True)
 os.chdir(wd)
-subprocess.call('python3 test_pesq.py', shell=True)
+subprocess.call('python test_pesq.py', shell=True)
