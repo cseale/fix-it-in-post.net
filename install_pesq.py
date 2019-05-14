07d7e2f50ba5f2d9803bdc81e06f5bc8fcef453b
rpytel1 <rafal.pytel1@wp.pl>
2019-05-14 15:58:52 +0200

    Introducing aggregated metric calculations

--- install_pesq.py
+++ install_pesq.py
@@ -1,7 +1,6 @@
 import os
 import subprocess
-# subprocess.run(["cd", "python_pesq/pypesq"])
-# subprocess.call('git submodule update', shell=True)
+
 wd = os.getcwd()
 os.chdir("pesq_lib/pypesq")
 subprocess.call('python setup.py build_ext --inplace', shell=True)
