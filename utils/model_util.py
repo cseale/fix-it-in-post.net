b2bb6ec94fa73592005c72707d25c168989d45b3
colm_seale <colm.seale@gmail.com>
2019-05-19 18:38:48 +0200

    some ibm edits

--- utils/model_util.py
+++ utils/model_util.py
@@ -4,7 +4,7 @@ from model.baseline_model import FullyConnectedBaseline as fcnetwork, Convolutio
 
 def load_model(n_features=129, n_segments=8, model_to_test="Baseline_FullyConnected/0505_130215", type="fc"):
     model = create_model(type, n_features, n_segments)
-    model_path = "./saved/" + model_to_test + "/model_best.pth"
+    model_path = "./saved/" + model_to_test + "/model_best.ibm.pth"
     model.load_state_dict(torch.load(model_path, map_location='cpu')['state_dict'])
 
     return model
