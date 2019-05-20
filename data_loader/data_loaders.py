487d69bc50381298a2181843ccb0f60aeaacf78d
rpytel1 <rafal.pytel1@wp.pl>
2019-05-20 16:35:19 +0200

    Updated data_loaders.py to work for both CNN and RNN

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -5,10 +5,13 @@ import boto3
 import botocore
 import os
 import getpass
-from process_rnn import process_audio, get_directory_name
+import process_rnn as rnnpr
+import process as pr
+
 from torch.utils.data.dataset import Dataset  # For custom datasets
 
 EDINBURGH_DATA_DIR = "./data/processed/edinburgh-noisy-speech-db/"
+
 
 class STFTDataLoader(BaseDataLoader):
     """
@@ -16,20 +19,26 @@ class STFTDataLoader(BaseDataLoader):
 
     Load preprocessed data
     """
-    def __init__(self, batch_size, shuffle=False, validation_split=0.0, num_workers=1, window_length = 256, overlap = 0.75, sampling_rate = 8e3, num_segments = 8):
-        self.dataset = EdinburghDataset(window_length, overlap, sampling_rate, num_segments)
+
+    def __init__(self, batch_size, shuffle=False, validation_split=0.0, num_workers=1, window_length=256, overlap=0.75,
+                 sampling_rate=8e3, num_segments=8, type="conv"):
+        self.dataset = EdinburghDataset(window_length, overlap, sampling_rate, num_segments, type)
         super(STFTDataLoader, self).__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
 
 
 class EdinburghDataset(Dataset):
 
-    def __init__(self, window_length, overlap, sampling_rate, num_segments): 
-        self.data_dir = get_directory_name(window_length, overlap, sampling_rate, num_segments)
-        
+    def __init__(self, window_length, overlap, sampling_rate, num_segments, type):
+        self.type = type
+        if type == "rnn":
+            self.data_dir = rnnpr.get_directory_name(window_length, overlap, sampling_rate, num_segments)
+        else:
+            self.data_dir = pr.get_directory_name(window_length, overlap, sampling_rate, num_segments)
+
         # keep track of data being loaded
         self.loaded_indices = []
         self.loaded_count = 0
-        
+
         # check if the correct data exists locally, or on s3. If neither, generate either
         self.use_s3 = False
         if checkIfDataExistsOnLocal(self.data_dir):
@@ -39,19 +48,33 @@ class EdinburghDataset(Dataset):
             print("data is on S3")
         else:
             print("no data, processing locally...")
-            process_audio(process_all=True, window_length = window_length, overlap = overlap, sampling_rate = sampling_rate, num_segments = num_segments)
+            if type == "rnn":
+                rnnpr.process_audio(process_all=True, window_length=window_length, overlap=overlap,
+                                    sampling_rate=sampling_rate, num_segments=num_segments)
+            else:
+                pr.process_audio(process_all=True, window_length=window_length, overlap=overlap,
+                                 sampling_rate=sampling_rate, num_segments=num_segments)
 
         # read the amount of samples we have
         self.length = readLengthFile(self.data_dir, self.use_s3)
-        num_features = int((window_length/2) + 1)
-        self.data = torch.zeros([self.length, num_features])
+        num_features = int((window_length / 2) + 1)
+        if type == "rnn":
+            self.data = torch.zeros([self.length, num_features])
+        else:
+            self.data = torch.zeros([self.length, num_features*num_segments])
+
         self.labels = torch.zeros([self.length, num_features])
 
     def __getitem__(self, index):
         # If all files have not been loaded, and the index being queried has not been loaded, then load the file
         if self.loaded_count != self.length and index not in self.loaded_indices:
             d = readSampleFile(self.data_dir, index, self.use_s3)
-            self.data[index] = torch.from_numpy(d['predictors'])
+
+            if self.type == "rnn":
+                self.data[index] = torch.from_numpy(d['predictors'])
+            else:
+                self.data[index] = torch.from_numpy(d['predictors']).reshape(1, -1)
+
             self.labels[index] = torch.from_numpy(d['targets'])
             self.loaded_indices.append(index)
             self.loaded_count = self.loaded_count + 1
@@ -69,18 +92,21 @@ def readLengthFile(data_dir, use_s3):
     else:
         return int(readLocalFile(data_dir, info_filename))
 
+
 def readSampleFile(data_dir, index, use_s3):
-    filename = "sample." + str(index) + ".pkl"   
+    filename = "sample." + str(index) + ".pkl"
     if use_s3:
         return pickle.loads(downloadS3File(data_dir, filename))
     else:
         return pickle.loads(readLocalFile(data_dir, filename))
+
 
 def readLocalFile(data_dir, filename):
     data_dir = EDINBURGH_DATA_DIR + data_dir
     with open(data_dir + filename, 'rb') as f:
         d = f.read()
     return d
+
 
 def downloadS3File(data_dir, filename):
     s3 = boto3.resource('s3')
@@ -89,6 +115,7 @@ def downloadS3File(data_dir, filename):
     d = obj.get().get('Body').read()
     return d
 
+
 def checkIfDataExistsOnS3(data_dir):
     s3 = boto3.resource('s3')
     try:
@@ -96,7 +123,8 @@ def checkIfDataExistsOnS3(data_dir):
         return True
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
-            return False 
+            return False
+
 
 def checkIfDataExistsOnLocal(data_dir):
     data_dir = EDINBURGH_DATA_DIR + data_dir
