bef2b3988e4a3ced30b67f862635ffeb99a87a08
bianca26 <biby_iancu26@yahoo.com>
2019-05-26 11:22:35 +0200

    fix names in model.py

--- model/model.py
+++ model/model.py
@@ -3,8 +3,10 @@ import torch.nn.functional as F
 from base import BaseModel
 from model.fully_connected import *
 from model.deep_fully_connected import *
-from model.convolutional import *
-from model.convolutional_timeline import *
+from model.convolutional_deep_time import *
+from model.convolutional_deep import *
+from model.convolutional_shallow import *
+from model.convolutional_shallow_time import *
 from model.gru import *
 from model.lstm import *
 
