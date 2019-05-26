4552eb56b64dbbfb5233f07f637c86aca3e84d04
colm_seale <colm.seale@gmail.com>
2019-05-26 10:05:17 +0200

    split models into seperate files

---
+++ model/lstm.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from base import BaseModel
+
+class baseLSTM(nn.Module):
+    def __init__(self, n_features, n_segments):
+        super(baseLSTM, self).__init__()
+        self.hidden_dim = 128
+        self.num_layers = 256
+
+        # Define the LSTM layer
+        self.lstm = nn.LSTMCell(n_features, self.hidden_dim, self.num_layers)
+
+        # Define the output layer
+        self.linear = nn.Linear(self.hidden_dim, n_features)
+
+    def forward(self, x):
+
+        if torch.cuda.is_available():
+            h0 = Variable(torch.zeros(self.hidden_dim, x.size(0), self.hidden_dim).cuda())
+        else:
+            h0 = Variable(torch.zeros(self.hidden_dim, x.size(0), self.hidden_dim))
+
+        hn = h0[0, :]
+
+        if torch.cuda.is_available():
+            c0 = Variable(torch.zeros(self.hidden_dim, x.size(0), self.hidden_dim).cuda())
+        else:
+            c0 = Variable(torch.zeros(self.hidden_dim, x.size(0), self.hidden_dim))
+
+        cn = c0[0, :]
+
+        hn, cn = self.lstm(x, (hn, cn))
+        out = self.linear(hn)
+
+        return out
